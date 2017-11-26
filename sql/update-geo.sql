f33df9692a867b7688159cbb82b1bd523e050cf7
lizhuo <lizhuogo@gmail.com>
2017-11-25 21:15:39 -0600

    update table content

---
+++ sql/update-geo.sql
@@ -0,0 +1,3 @@
+UPDATE `university_geo`
+SET `latitude`=39.3267914, `longitude`=-76.6194588, `country`='United States of America', `country_code`='us', `continent`='North America', `continent_code`='na'
+WHERE `id`=17;
